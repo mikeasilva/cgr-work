6c0eecaff12f6ba32ccff4ad2738f0e90f257520
Mike Silva <mike.a.silva@gmail.com>
2014-04-10 16:57:17 -0400

    Updated X12 NYSDOL R Script
    Getting closer on the X-12-ARIMA Seasonal Adjustment Script

--- sa-nysdol-employment-data/X12 NYSDOL CES Data.R
+++ sa-nysdol-employment-data/X12 NYSDOL CES Data.R
@@ -1,14 +1,18 @@
+# Load the libraries needed for the analysis
+library(reshape2)
+library(x12)
+# Set path to x12-ARIMA binary
+x12path("C:/x12a/x12a.exe")
+
 # Download data from http://www.labor.state.ny.us/stats/ces.zip and load it into a data frame
 temp.file <- tempfile()
 download.file("http://www.labor.state.ny.us/stats/ces.zip",temp.file)
 data <- read.csv(unz(temp.file, "ces_naics.txt"))
 unlink(temp.file)
-step.1 <- nrow(data)
 
 # Select the subset by series code
 seriescode.selection <- c(0, 15000000, 10000000, 20000000, 30000000, 41000000, 42000000, 43000000, 50000000, 55000000, 60000000, 65000000, 70000000, 80000000, 90000000)
 data <- data[data$SERIESCODE %in% seriescode.selection, ]
-step.2 <- nrow(data)
 
 # Recode area
 myarea <- function(x){
@@ -84,7 +88,7 @@ myseries <- function(x){
 }
 data$myseries <- sapply(data$SERIESCODE, myseries)
 
-# Concatenate area ans series to form mycode
+# Concatenate area and series to form mycode
 data$mycode <- paste(data$myarea,data$myseries, sep="")
 
 # Rename variables
@@ -107,24 +111,41 @@ drops <- c("SERIESCODE","INDUSTRY_TITLE", "AREATYPE", "AREA", "AREANAME", "ANNUA
 data <- data[,!(names(data) %in% drops)]
 
 # Normalize the data
-library(reshape2)
 mdata <- melt(data, id = c("mycode","year"), variable.name = "month", na.rm=TRUE)
 mdata$mydate <- paste(mdata$year,"-",mdata$month,"-01", sep="")
 mdata$mydate <- as.Date(mdata$mydate, "%Y-%m-%d")
 mdata <- mdata[order(as.Date(mdata$mydate, format="%Y-%m-%d")),]
 nonSA.data <- dcast(mdata, year + month ~ mycode, value.var = "value", fun.aggregate=sum)
-#nonSA2.data <- dcast(mdata, mycode + year ~ month, value.var = "value", fun.aggregate=sum)
 
 # Write the file
 write.csv(nonSA.data, file = "NYS Metro CES nonSA.csv")
-#write.csv(nonSA2.data, file = "NYS Metro CES nonSA2.csv")
 
-# Hack right now
-# I need to step through all the series
-newdata <- mdata[ which(mdata$mycode=='nystot'), ]
-employ<-ts(newdata$value,frequency=12,start(1990,1))
-deseason<-stl(employ,s.window="periodic")
+# Initialize seasonally adjusted data frame
+sa.df <- data.frame(year=as.Date(character()), month=character(), value=character(), mycode=character()) 
+# Get the list of mycode
+mycodes <- c("albehs",  "albfin",  "albgov",  "albinf",	"alblei",	"albman",	"albnmc",	"alboth",	"albpbs",	"albtot",	"albtra",	"albtwu",	"binehs",	"binfin",	"bingov",	"bininf",	"binlei",	"binman",	"binnmc",	"binoth",	"binpbs",	"bintot",	"bintra",	"bintwu",	"bufehs",	"buffin",	"bufgov",	"bufinf",	"buflei",	"bufman",	"bufnmc",	"bufoth",	"bufpbs",	"buftot",	"buftra",	"buftwu",	"elmehs",	"elmfin",	"elmgov",	"elminf",	"elmlei",	"elmman",	"elmnmc",	"elmoth",	"elmpbs",	"elmtot",	"elmtra",	"elmtwu",	"gleehs",	"glefin",	"glegov",	"gleinf",	"glelei",	"gleman",	"glenmc",	"gleoth",	"glepbs",	"gletot",	"gletra",	"gletwu",	"ithehs",	"ithfin",	"ithgov",	"ithinf",	"ithlei",	"ithman",	"ithnmc",	"ithoth",	"ithpbs",	"ithtot",	"ithtra",	"ithtwu",	"kinehs",	"kinfin",	"kingov",	"kininf",	"kinlei",	"kinman",	"kinnmc",	"kinoth",	"kinpbs",	"kintot",	"kintra",	"kintwu",	"liehs",	"lifin",	"ligov",	"liinf",	"lilei",	"liman",	"linmc",	"lioth",	"lipbs",	"litot",	"litra",	"litwu",	"nycehs",	"nycfin",	"nycgov",	"nycinf",	"nyclei",	"nycman",	"nycnmc",	"nycoth",	"nycpbs",	"nyctot",	"nyctra",	"nyctwu",	"nysehs",	"nysfin",	"nysgov",	"nysinf",	"nyslei",	"nysman",	"nysnmc",	"nysoth",	"nyspbs",	"nystot",	"nystra",	"nystwu",	"pouehs",	"poufin",	"pougov",	"pouinf",	"poulei",	"pouman",	"pounmc",	"pouoth",	"poupbs",	"poutot",	"poutra",	"poutwu",	"prwehs",	"prwfin",	"prwgov",	"prwinf",	"prwlei",	"prwman",	"prwnmc",	"prwoth",	"prwpbs",	"prwtot",	"prwtra",	"prwtwu",	"rocehs",	"rocfin",	"rocgov",	"rocinf",	"roclei",	"rocman",	"rocnmc",	"rocoth",	"rocpbs",	"roctot",	"roctra",	"roctwu",	"syrehs",	"syrfin",	"syrgov",	"syrinf",	"syrlei",	"syrman",	"syrnmc",	"syroth",	"syrpbs",	"syrtot",	"syrtra",	"syrtwu",	"utiehs",	"utifin",	"utigov",	"utiinf",	"utilei",	"utiman",	"utinmc",	"utioth",	"utipbs",	"utitot",	"utitra",	"utitwu")
+# Loop through the codes
+for (i in 1:length(mycodes)) {
+  # X-12-ARIMA loop
+  temp.data <- mdata[ which(mdata$mycode==mycodes[i]), ]
+  min.year <- min(temp.data$year)
+  # Get the employment time series
+  employ<-ts(temp.data$value,frequency=12,start(min.year,1))
+  # Apply the X-12 seasonal adjustment
+  x12.employ <- x12(employ)
+  # Get the seasonally adjusted varriable
+  sa.employ <- x12.employ@d11
+  sa <- tapply(sa.employ, list(year = floor(time(sa.employ)), month = month.abb[cycle(sa.employ)]), c)
+  mdata2 <- melt(sa, id = c("year"), variable.name = "month", na.rm=TRUE)
+  mdata2$year <- mdata2$year + min.year - 1
+  mdata2$mycode <- mycodes[i]
+  sa.df <- rbind(sa.df, mdata2)
+}
 
-library("x12")
-x12path("C:/x12a/x12a.exe")
-sa.employ <- x12(employ)
+# Recode month
+sa.df$month <- match(sa.df$month, month.abb)
+sa.df$mydate <- paste(sa.df$year,"-",sa.df$month,"-01", sep="")
+sa.df$mydate <- as.Date(sa.df$mydate, "%Y-%m-%d")
+SA.data <- dcast(sa.df, mydate + year + month ~ mycode, value.var = "value", fun.aggregate=sum)
+SA.data <- SA.data[,!(names(data) %in% c("mydate"))]
+write.csv(SA.data, file = "NYS Metro CES SA.csv")
