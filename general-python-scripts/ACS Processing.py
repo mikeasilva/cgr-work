422e320f5f7efcf4f489bbf7e38a01db0063bbb3
Mike Silva <mike.a.silva@gmail.com>
2014-12-10 11:31:43 -0500

    Fixed ACS Processing Python Script
    Created a function to get the ids

--- general-python-scripts/ACS Processing.py
+++ general-python-scripts/ACS Processing.py
@@ -20,47 +20,44 @@ def get_worksheet(file_path, sheet_name):
 ## This function
 def process_acs_spreadsheet(file_name, ids, root_dir):
     ids_to_find = len(ids)
-    file_path = root_dir + 'NY/' + file_name
-    print(str(datetime.datetime.now().time()) + ' <- Before opening ' + file_path)
-    ws = open_workbook(file_path).sheet_by_index(0)
-    print(str(datetime.datetime.now().time()) + ' <- After opening ' + file_path)
+    print(str(datetime.datetime.now().time()) + ' <- Searching for ' + str(ids_to_find) + ' geographies in '+file_name)
+    if ids_to_find > 0:
+        # Open the spreadsheet (this takes a while)
+        file_path = root_dir + 'NY/' + file_name
+        print(str(datetime.datetime.now().time()) + ' <- Opening the file')
+        ws = open_workbook(file_path).sheet_by_index(0)
 
-    new_file_path = root_dir + 'Processed/' + file_name
-    new_wb = xlsxwriter.Workbook(new_file_path)
-    new_ws = new_wb.add_worksheet()
+        # Create the new spreadsheet
+        new_file_path = root_dir + 'Processed/' + file_name
+        new_wb = xlsxwriter.Workbook(new_file_path)
+        new_ws = new_wb.add_worksheet()
+        # Initialize vars
+        ids_found = 0
+        for row in range(ws.nrows):
+            if ids_found < ids_to_find:
+                lookup_id = ws.cell(row, 5).value
+                if lookup_id in ids:
+                    for col in range(ws.ncols):
+                        new_ws.write(ids_found, col, ws.cell(row, col).value)
+                    ids.remove(lookup_id)
+                    ids_found = ids_found + 1
+        new_wb.close()
+        print(str(datetime.datetime.now().time()) + ' <- Done')
 
-    ids_found = 0
-
+def get_ids(root_dir):
+    ## Get the list of ids we need to look for
+    ids_data = []
+    file_path = root_dir + 'ny-ids.xlsx'
+    ws = get_worksheet(file_path, '2009-13 ACS')
+    col=0
     for row in range(ws.nrows):
-        if ids_found < ids_to_find:
-            row_id = ws.cell(row, 5).value
-            if row_id in ids:
-                for col in range(ws.ncols):
-                    new_ws.write(ids_found, col, ws.cell(row, col).value)
-                ids.remove(row_id)
-                ids_found = ids_found + 1
+        ids_data.append(ws.cell(row, col).value)
+    return ids_data
 
-    new_wb.close()
+dir_path = join(root_dir, 'NY')
+## Get the files in the sub directory
+files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
 
-## Get the list of ids we need to look for
-ids_data = []
-file_path = root_dir + 'ids.xlsx'
-print(str(datetime.datetime.now().time())+' <- Before opening ids.xlsx')
-ws = get_worksheet(file_path, '2009-13 ACS')
-print(str(datetime.datetime.now().time())+' <- After opening ids.xlsx')
-col=0
-for row in range(ws.nrows):
-    ids_data.append(ws.cell(row, col).value)
-
-ids_to_find = len(ids_data)
-
-all_subdirs = ['NY']
-## Loop through all the sub directories
-for d in all_subdirs:
-    dir_path = join(root_dir, d)
-    ## Get the files in the sub directory
-    files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
-    ## Loop through each file
-    for file_name in files:
-        print(str(datetime.datetime.now().time())+' <- Starting on '+file_name)
-        process_acs_spreadsheet(file_name, ids_data, root_dir)
+for file_name in files:
+    ids = get_ids(root_dir)
+    process_acs_spreadsheet(file_name, ids, root_dir)
