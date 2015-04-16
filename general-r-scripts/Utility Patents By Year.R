e00670298636e39d51652c6a24b82e52cc8fe6c1
Mike Silva <mike.a.silva@gmail.com>
2015-04-16 17:33:57 -0400

    Update Metro Patents

--- general-r-scripts/Utility Patents By Year.R
+++ general-r-scripts/Utility Patents By Year.R
@@ -2,6 +2,11 @@
 ## This script scrapes the PTO's website for their utility patents by year and
 ## creates and excel file 
 
+library(dplyr)
+library(rvest)
+library(reshape2)
+library(xlsx)
+
 ## Value & Variable Names
 val.name <- 'Utility Patents'
 var.name <- 'Year'
@@ -9,19 +14,15 @@ var.name <- 'Year'
 file.path <- 'H:/Data Warehouse/US Patent and Trademark Office (USPTO)/Utility Patents By Year.xlsx'
 
 ## Scrape the web for utility patent grant figures
-library(rvest) # Load the package
 url <- 'http://www.uspto.gov/web/offices/ac/ido/oeip/taf/countyall/usa_county_gd.htm'
+
 pto <- html(url) %>%
   html_nodes('table') %>%
-  html_table()
-
-pto <- pto[[1]]
-
-## Remove the total column
-pto <- pto[!names(pto) %in% c('Total') ]
+  html_table() %>%
+  as.data.frame(.) %>%
+  select(-Total)
 
 ## Reshape from wide to long
-library(reshape2) # Load the package
 pto <- melt(pto, id.vars = names(pto)[1:4], variable.name=var.name, 
             value.name=val.name)
 
@@ -29,7 +30,6 @@ pto <- melt(pto, id.vars = names(pto)[1:4], variable.name=var.name,
 pto <- pto[,c(names(pto)[1:4], val.name, var.name)]
 
 ## Save the PTO data
-library(xlsx) # Load the package
 write.xlsx(x=pto, file=file.path, sheetName='USPTO_Utility_Patents_By_Year', 
            row.names = FALSE)
 # The quicker alternative
